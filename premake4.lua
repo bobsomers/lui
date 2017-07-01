55078dab6453ea486050b095d028f41d75c3a183
Bob Somers <bob@bobsomers.com>
2017-07-01 12:38:24 -0700

    Vendoring NanoVG.

--- premake4.lua
+++ premake4.lua
@@ -1,18 +1,38 @@
-solution "Thermyte"
+solution "thermyte"
+  location "build"
   configurations { "Debug", "Release" }
+
+  project "nanovg"
+    language "C"
+    kind "StaticLib"
+    includedirs { "deps/nanovg/src" }
+    files { "deps/nanovg/src/*.c" }
+    targetdir "build"
+
+    configuration "Debug"
+      defines { "DEBUG" }
+      flags { "Symbols", "ExtraWarnings" }
+
+    configuration "Release"
+      defines { "NDEBUG" }
+      flags { "OptimizeSpeed", "ExtraWarnings" }
 
   project "thermyte"
     kind "ConsoleApp"
     language "C"
-    files {
-      "src/**.h",
-      "src/**.c"
-    }
+    includedirs { "src", "deps/nanovg/src" }
+    files { "src/*.c" }
+    targetdir "build"
+    links { "nanovg" }
 
     configuration "Debug"
       defines { "DEBUG" }
-      flags { "Symbols" }
+      flags { "Symbols", "ExtraWarnings" }
 
     configuration "Release"
       defines { "NDEBUG" }
-      flags { "Optimize", "Symbols" }
+      flags { "OptimizeSpeed", "Symbols", "ExtraWarnings" }
+
+    configuration { "maxosx" }
+      links { "glfw" }
+      linkoptions { "-framework OpenGL", "-framework Cocoa", "-framework IOKit", "-framework CoreVideo", "-framework Carbon" }
