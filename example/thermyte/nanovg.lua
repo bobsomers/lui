4e9498d8e4c5152787f3fda11ff6e8ba4f27b3b7
Bob Somers <bob@bobsomers.com>
2017-07-09 01:55:07 -0700

    Added more API declarations for NanoVG.

--- example/thermyte/nanovg.lua
+++ example/thermyte/nanovg.lua
@@ -10,6 +10,8 @@ ffi.cdef [[
   /*
    * ================= TYPES ==================================================
    */
+  typedef struct NVGcontext {} NVGcontext;
+
   typedef struct NVGcolor {
     union {
       float rgba[4];
@@ -18,6 +20,16 @@ ffi.cdef [[
       };
     };
   } NVGcolor;
+
+  typedef struct NVGpaint {
+    float xform[6];
+    float extent[2];
+    float radius;
+    float feather;
+    NVGcolor innerColor;
+    NVGcolor outerColor;
+    int image;
+  } NVGpaint;
 
   typedef enum NVGalign {
     NVG_ALIGN_LEFT = 1<<0,
@@ -45,7 +57,23 @@ ffi.cdef [[
     NVG_ALIGN_RIGHT_BASELINE = NVG_ALIGN_RIGHT | NVG_ALIGN_BASELINE,
   } NVGalign;
 
-  typedef struct NVGcontext {} NVGcontext;
+  typedef enum NVGwinding {
+    NVG_CCW = 1,
+    NVG_CW = 2,
+  } NVGwinding;
+
+  typedef enum NVGsolidity {
+    NVG_SOLID = 1,
+    NVG_HOLE = 2,
+  } NVGsolidity;
+
+  typedef enum NVGlineCap {
+    NVG_BUTT,
+    NVG_ROUND,
+    NVG_SQUARE,
+    NVG_BEVEL,
+    NVG_MITER,
+  } NVGlineCap;
 
   /*
    * ================= COLOR UTILS ============================================
@@ -72,13 +100,54 @@ ffi.cdef [[
    * ================= RENDER STYLES ==========================================
    */
   void nvgStrokeColor(NVGcontext* ctx, NVGcolor color);
+  void nvgStrokePaint(NVGcontext* ctx, NVGpaint paint);
   void nvgFillColor(NVGcontext* ctx, NVGcolor color);
+  void nvgFillPaint(NVGcontext* ctx, NVGpaint paint);
+  void nvgMiterLimit(NVGcontext* ctx, float limit);
+  void nvgStrokeWidth(NVGcontext* ctx, float size);
+  void nvgLineCap(NVGcontext* ctx, NVGlineCap cap);
+  void nvgLineJoin(NVGcontext* ctx, NVGlineCap join);
+  void nvgGlobalAlpha(NVGcontext* ctx, float alpha);
+
+  /*
+   * ================= PAINTS =================================================
+   */
+  NVGpaint nvgLinearGradient(NVGcontext* ctx, float sx, float sy, float ex,
+                             float ey, NVGcolor icol, NVGcolor ocol);
+  NVGpaint nvgBoxGradient(NVGcontext* ctx, float x, float y, float w, float h,
+                          float r, float f, NVGcolor icol, NVGcolor ocol);
+  NVGpaint nvgRadialGradient(NVGcontext* ctx, float cx, float cy, float inr,
+                             float outr, NVGcolor icol, NVGcolor ocol);
+  NVGpaint nvgImagePattern(NVGcontext* ctx, float ox, float oy, float ex,
+                           float ey, float angle, int image, float alpha);
+
+  /*
+   * ================= SCISSORING =============================================
+   */
+  void nvgScissor(NVGcontext* ctx, float x, float y, float w, float h);
+  void nvgIntersectScissor(NVGcontext* ctx, float x, float y, float w, float h);
+  void nvgResetScissor(NVGcontext* ctx);
 
   /*
    * ================= PATHS ==================================================
    */
   void nvgBeginPath(NVGcontext* ctx);
+  void nvgMoveTo(NVGcontext* ctx, float x, float y);
+  void nvgLineTo(NVGcontext* ctx, float x, float y);
+  void nvgBezierTo(NVGcontext* ctx, float c1x, float c1y, float c2x, float c2y,
+                   float x, float y);
+  void nvgQuadTo(NVGcontext* ctx, float cx, float cy, float x, float y);
+  void nvgArcTo(NVGcontext* ctx, float x1, float y1, float x2, float y2, float radius);
+  void nvgClosePath(NVGcontext* ctx);
+  void nvgPathWinding(NVGcontext* ctx, int dir);
+  void nvgArc(NVGcontext* ctx, float cx, float cy, float r, float a0, float a1, int dir);
   void nvgRect(NVGcontext* ctx, float x, float y, float w, float h);
+  void nvgRoundedRect(NVGcontext* ctx, float x, float y, float w, float h, float r);
+  void nvgRoundedRectVarying(NVGcontext* ctx, float x, float y, float w, float h,
+                             float radTopLeft, float radTopRight,
+                             float radBottomRight, float radBottomLeft);
+  void nvgEllipse(NVGcontext* ctx, float cx, float cy, float rx, float ry);
+  void nvgCircle(NVGcontext* ctx, float cx, float cy, float r);
   void nvgFill(NVGcontext* ctx);
   void nvgStroke(NVGcontext* ctx);
 
@@ -88,8 +157,12 @@ ffi.cdef [[
   int nvgCreateFont(NVGcontext* ctx, const char* name, const char* filename);
   void nvgFontFaceId(NVGcontext* ctx, int font);
   void nvgFontSize(NVGcontext* ctx, float size);
+  void nvgFontBlur(NVGcontext* ctx, float blur);
+  void nvgTextLetterSpacing(NVGcontext* ctx, float spacing);
+  void nvgTextLineHeight(NVGcontext* ctx, float lineHeight);
   void nvgTextAlign(NVGcontext* ctx, NVGalign align);
   float nvgText(NVGcontext* ctx, float x, float y, const char* string, const char* end);
+  void nvgTextBox(NVGcontext* ctx, float x, float y, float breakRowWidth, const char* string, const char* end);
 ]]
 
 return setmetatable(nanovg, {__index = ffi.C})
